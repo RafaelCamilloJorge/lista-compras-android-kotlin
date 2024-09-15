ea73fad891c75ddd8069cb4cdf10906bf0105ae9
Rafael Jorge <rafaelcamillojorge@gmail.com>
2024-09-14 21:14:14 -0300

    Add login validation and error message display

--- app/src/main/java/com/example/listadecompras/domain/ShoppingItem.kt
+++ app/src/main/java/com/example/listadecompras/domain/ShoppingItem.kt
@@ -1,17 +1,5 @@
 class ShoppingItem(
     var name: String,
-    var quatity: Int = 0,
+    var quantity: Int = 0,
     var unity: UnitOfMeasure,
-) {
-    public fun getName(): String {
-        return name;
-    }
-
-    public fun getQuantity(): Int {
-        return quatity;
-    }
-
-    public fun getUnit(): UnitOfMeasure {
-        return unity;
-    }
-}
+)
--- app/src/main/java/com/example/listadecompras/feature/login/MainActivity.kt
+++ app/src/main/java/com/example/listadecompras/feature/login/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.listadecompras.feature.login
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.listadecompras.R
 import com.example.listadecompras.databinding.ActivityMainBinding
 import com.example.listadecompras.feature.register.RegisterActivity
+import com.example.listadecompras.feature.shopping_lists.ShoppingListActivity
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val loginViewModel = LoginViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +28,47 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        binding.loginButton.setOnClickListener {
+            val username = binding.emailField.text.toString()
+            val password = binding.passwordField.text.toString()
+
+
+            if (username.isBlank() || password.isBlank()) {
+                showErrorMsg("É necessário preencher o usuário e a senha.")
+            } else {
+                val result = loginViewModel.login(username, password)
+
+                when (result) {
+                    is OnResult.Success -> {
+                        val intent = Intent(this, ShoppingListActivity::class.java)
+                        startActivity(intent)
+                    }
+                    is OnResult.Error -> {
+                        showErrorMsg("Credenciais inválidas.")
+                    }
+                    else -> {
+                        showErrorMsg("Erro ao tentar logar.")
+                    }
+                }
+            }
+        }
+
         binding.registerButton.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
     }
+
+    private fun showErrorMsg(errorMessage: String) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setMessage(errorMessage)
+            .setTitle("Erro ao logar")
+            .setNeutralButton("Ok") { dialog, _ ->
+                dialog.dismiss()
+            }
+
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
 }

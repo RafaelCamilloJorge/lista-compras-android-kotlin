32d7e512969c9c236d040f96d37d09e65f9b9b51
Rafael Jorge <rafaelcamillojorge@gmail.com>
2024-09-22 15:04:25 -0300

    rename files and update manifest

--- app/src/main/java/com/example/listadecompras/feature/manage_list/ManageListActivity.kt
+++ app/src/main/java/com/example/listadecompras/feature/manage_list/ManageListActivity.kt
@@ -6,12 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.listadecompras.R
+import com.example.listadecompras.databinding.ActivityManageListBinding
 
-class ManegeListActivity : AppCompatActivity() {
+class ManageListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityManageListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_manege_list)
+        binding = ActivityManageListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
